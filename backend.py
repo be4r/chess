2b7c2de35058c54330b8fe9673d1a7379721ed7e
Dmitriy Konovalchuk <02.03.1998@list.ru>
2021-06-07 15:37:56 +0300

    Add main structure of possible pieces moves search

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
 def check_if_move_correct(self.board, move):
-    pass
+    if move in Board.get_all_possible_moves():    
+        return True
+    return False
 
 def add_move_to_board(self.board, move):
     pass
@@ -7,11 +9,12 @@ def add_move_to_board(self.board, move):
 def check_if_end_of_game(self.board, move):
     pass
 
+
 class Board:
-    def __init__(...):
+    def __init__(self):
         '''
             Инициализация доски. Фигуры располагаются в порядке, установленном
-            правилами шахмат.
+            правилами шахмат (от 15 века нашей эры).
         '''
         self.pieces_positions = []
         
@@ -28,9 +31,64 @@ class Board:
 
         self.is_check = False
         self.is_mate = False
+        self.active_player = 0
+
 
     def get_pieces_positions(...):
         return self.pieces_positions
 
-    def _get_all_possible_mooves(...):
+    def get_all_possible_moves(...):
+        '''
+            Вычисление всех возможных ходов, которые может сделать игрок. Учитывается положение фигур на доске и очерёдность хода игроков. 
+        '''
+        all_possible_moves = []
+        for piece_raw_ind in range(len(self.pieces_positions)):
+            for piece_col_ind, piece_name in enumerate(range(len(self.pieces_positions[piece_raw_ind]))):
+                piece_possible_moves = self.get_piece_possible_moves(piece_name, piece_raw_ind, piece_col_ind)
+                all_possible_moves += piece_possible_moves
+        return all_possible_moves
+
+        
+    def get_piece_possible_moves(self, piece_name: str, piece_raw_ind: int, piece_col_ind: int):
+        '''
+            Вычисление всех возможных ходов заданной фигуры.
+            Учитывается то, что: 
+            * фигура может поставить шах
+            * фигура, переместившись, может поставить своего короля под шах
+            * фигуры не могут прыгать через другие фигуры (кроме коней --- они могут)
+            
+        '''
+        if int(piece_name[-1]) != self.active_player:
+            return []
+        piece_possible_moves = []
+
+        if piece_name[:-1] == "pawn":
+            pass
+        elif piece_name[:-1] == "rook":
+            pass
+        elif piece_name[:-1] == "king":
+            pass
+        elif piece_name[:-1] == "queen":
+            pass
+        elif piece_name[:-1] == "knight":
+            pass
+        elif piece_name[:-1] == "bishop":
+            pass
+
+        checked_piece_possible_moves = []
+        for possible_move in piece_possible_moves:
+            if not self.is_check_after_move(possible_move):
+                checked_piece_possible_moves.append(possible_move)
+
+        return checked_piece_possible_moves
+
+    def is_check_after_move(move):
+        
+
+    def check_if_check(self):
+        pass
+
+
+    def 
+
         
