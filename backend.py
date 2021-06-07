79b29557fa90a0243b44107f1ffe6db7052a2231
Dmitriy Konovalchuk <02.03.1998@list.ru>
2021-06-07 13:54:50 +0300

    Add board initialization and getter-function

--- backend.py
+++ backend.py
@@ -9,5 +9,28 @@ def check_if_end_of_game(self.board, move):
 
 class Board:
     def __init__(...):
-        pass
+        '''
+            Инициализация доски. Фигуры располагаются в порядке, установленном
+            правилами шахмат.
+        '''
+        self.pieces_positions = []
+        
+        # Добавление чёрных фигур
+        self.pieces_positions.append(["rook1", "knight1", "bishop1", "queen1", "king1", "bishop1", "knight1", "rook1"])
+        self.pieces_positions.append(["pawn1" for i in range(8)])
 
+        # Добавление пустых полей        
+        self.pieces_positions += [["empty" for i in range(8)] for i in range 4]
+
+        # Добавление белых фигур        
+        self.pieces_positions.append(["pawn0" for i in range(8)])
+        self.pieces_positions.append(["rook0", "knight0", "bishop0", "queen0", "king0", "bishop0", "knight0", "rook0"])                
+
+        self.is_check = False
+        self.is_mate = False
+
+    def get_pieces_positions(...):
+        return self.pieces_positions
+
+    def _get_all_possible_mooves(...):
+        
